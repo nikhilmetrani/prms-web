f71513ef19951b28c23cc5bc6502f412187a0fe2
Rushabh Shah <>
2015-09-23 10:31:20 +0800

    List of Radio programs added in the maintain schedule page

--- PRMS/src/java/sg/edu/nus/iss/phoenix/schedule/controller/ViewScheduleCmd.java
+++ PRMS/src/java/sg/edu/nus/iss/phoenix/schedule/controller/ViewScheduleCmd.java
@@ -8,9 +8,12 @@ package sg.edu.nus.iss.phoenix.schedule.controller;
 import at.nocturne.api.Action;
 import at.nocturne.api.Perform;
 import java.io.IOException;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import sg.edu.nus.iss.phoenix.radioprogram.delegate.ReviewSelectProgramDelegate;
+import sg.edu.nus.iss.phoenix.radioprogram.entity.RadioProgram;
 import sg.edu.nus.iss.phoenix.schedule.delegate.ScheduleDelegate;
 
 /**
@@ -30,6 +33,9 @@ public class ViewScheduleCmd implements Perform{
         }
         ScheduleDelegate delegate = new ScheduleDelegate();
         req.getSession().setAttribute("annualScheduleList", delegate.reviewSelectAnnualSchedule());
+        ReviewSelectProgramDelegate del = new ReviewSelectProgramDelegate();
+        List<RadioProgram> radioPrograms = del.reviewSelectRadioProgram();     
+        req.getSession().setAttribute("radioPgms", radioPrograms);
         return "/pages/maintainSchedule/setupSchedule.jsp";
     }
 }
